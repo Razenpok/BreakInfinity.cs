24f57ca971a1d62715f86356ad5fb28504ca0de7
Nyan cat <a0939712328@gmail.com>
2020-02-02 20:02:37 +0800

    Added string parse test case

--- BreakInfinity.Tests/Tests.cs
+++ BreakInfinity.Tests/Tests.cs
@@ -35,5 +35,15 @@ namespace BreakInfinity.Tests
             Assert.That(TestValueExponent1.ToString("F0"), Is.EqualTo("1235"));
             Assert.That(TestValueExponent1.ToString("F4"), Is.EqualTo("1234.5679"));
         }
+        
+        [Test]
+        public void TestStringParse()
+        {
+            string[] testCases = ["1e308", "1E308", "1E+308", "1e+308", "e308", "e+308"]
+            foreach (string testCase in testCases) {
+                BigDouble parsedNumber = BigDouble.Parse(testCase)
+                Assert.That(parsedNumber.ToDouble(), Is.EqualTo(Double.Parse(testCase)))
+            }
+        }
     }
 }
